2d70bb8e0ae3e05bbe12098a29ec35360f77afb0
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-04-25 13:37:50 +1000

    Modify Siadebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,33 +9,25 @@ export default class Sidebar extends Component {
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/image-profile.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Lawrence Shen</a></h1>
-              <span className="email"><i className="icon-mail"></i> lawrenceshen1213@gmail.com</span>
+              <h1 id="colorlib-logo"><a href="index.html">LAHAM</a></h1>
+              <h3>Project Management Consultancy</h3>
+              <span className="email"><i className="icon-mail"></i> lahampm@gmail.com</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
-                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  <li><a href="#about" data-nav-section="about">About US</a></li>
                   {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
                   <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
-                  <li><a href="#timeline" data-nav-section="timeline">Experience</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>
+                  <li><a href="#timeline" data-nav-section="timeline">Products</a></li>
+                  <li>Activities</li>
+                  <li><a href="#" data-nav-section="blog">Testimonial</a></li>
                 </ul>
               </div>
             </nav>
-            <nav id="colorlib-main-menu">
-              <ul>
-                <li><a href="https://www.linkedin.com/in/lawrence-shen-9b23791a4/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/lawrieshen" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@lawrenceshen1213" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
-              </ul>
-            </nav>
-            <div className="colorlib-footer">
-              <p><small>
-                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
-              </small></p>
-            </div>
+            
+          
           </aside>
         </div>
       </div>
