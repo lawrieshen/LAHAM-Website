8d4495664a5ed009bcdce46d73bfb050b6b036d1
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-04-25 18:05:46 +1000

    Change background image

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -6,7 +6,7 @@ function Introduction() {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/image-bg-1.avif)'}}>
+              <li style={{backgroundImage: 'url(images/laham-bg-1.avif)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -14,13 +14,16 @@ function Introduction() {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1  style={{color: "#fff", textShadow: "1px 1px 2px grey"}}>Hi! <br />We are LAHAM.</h1>
+                          {/*<nav>
+                            <p><a className="btn btn-primary btn-learn" href="#about" data-nav-section="about" rel="noopener noreferrer" style={{color: "#fff", border: "1px solid #fff", textShadow: "1px 1px 2px grey", boxShadow: "1px 1px 2px grey"}}>About Us <i className="icon-book" /></a></p>
+                          </nav>*/}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/image-bg-2.avif)'}}>
+              <li style={{backgroundImage: 'url(images/laham-bg-2.avif)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -28,22 +31,22 @@ function Introduction() {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1 style={{color: "#fff", textShadow: "1px 1px 2px grey"}}>We're experts at <br /> MULTIMEDIA PROJECTS</h1>
-                          <p><a className="btn btn-primary btn-learn" href="#blog" target="_blank" rel="noopener noreferrer" style={{color: "#fff", border: "1px solid #fff", textShadow: "1px 1px 2px grey", boxShadow: "1px 1px 2px grey"}}><b>View Product </b><i className="icon-briefcase3" /></a></p>
+                          {/*<p><a className="btn btn-primary btn-learn" href="#blog" target="_blank" rel="noopener noreferrer" style={{color: "#fff", border: "1px solid #fff", textShadow: "1px 1px 2px grey", boxShadow: "1px 1px 2px grey"}}><b>View Product </b><i className="icon-briefcase3" /></a></p>*/}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/image-bg-3.avif)'}}>
+              <li style={{backgroundImage: 'url(images/laham-bg-3.avif)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>We <br/>Transform ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://medium.com/@lawrenceshen1213" target="_blank" rel="noopener noreferrer">See our PMF and Lifecycles<i className="icon-book" /></a></p>
+                          <h1 style={{color: "#fff", textShadow: "1px 1px 2px grey"}}>We <br/>Transform ... </h1>
+                          {/*<p><a className="btn btn-primary btn-learn" href="https://medium.com/@lawrenceshen1213" target="_blank" rel="noopener noreferrer" style={{color: "#fff", border: "1px solid #fff", textShadow: "1px 1px 2px grey", boxShadow: "1px 1px 2px grey"}}>See our PMF and Lifecycles<i className="icon-book" /></a></p>*/}
                         </div>
                       </div>
                     </div>
